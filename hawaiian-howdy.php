6757aeec81ad3cc7ec8fc38912e407fcf97c7a16
Geoffrey Shilling <geoffrey@geoffreyshilling.com>
2017-06-12 20:09:28 -0400

    Add Hawaiian Howdy submenu and start work on options page.

--- hawaiian-howdy.php
+++ hawaiian-howdy.php
@@ -3,7 +3,7 @@
 Plugin Name: Hawaiian Howdy
 Plugin URI: 	https://geoffreyshilling.com/plugins/hawaiian-howdy/
 Description: 	Hawaiianizes the "Howdy" message displayed in the top right corner for users when they are logged in, based on time of day.
-Version: 		1.0.2
+Version: 		1.1.2
 Author: 		Geoffrey Shilling
 Author URI: 	https://geoffreyshilling.com/
 License:     	GPL2
@@ -24,11 +24,24 @@ You should have received a copy of the GNU General Public License
 along with Hawaiian Howdy. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
+if ( ! defined( 'HAWAIIAN_HOWDY_FILE' ) ) {
+	define( 'HAWAIIAN_HOWDY_FILE', __FILE__ );
+}
+
+// Load Hawaiian Howdy options page.
+require_once( dirname( HAWAIIAN_HOWDY_FILE ) . '/options.php' );
+
+if( is_admin() )
+    $my_settings_page = new MySettingsPage();
 
 class WPA_Hawaiian_Howdy {
   function __construct() {
 	   add_filter( 'admin_bar_menu', array( $this, 'hawaiianize_howdy' ), 25 );
-  }
+
+          add_action('admin_menu', 'wporg_options_page');
+          add_action('admin_menu', 'wpdocs_my_plugin_menu');
+          add_action('admin_menu', 'add_custom_link_into_appearnace_menu');
+            }
 
   //References  https://codex.wordpress.org/Function_Reference/get_node
   //https://wordpress.stackexchange.com/questions/227957/i-changed-howdy-in-the-admin-bar-in-the-dashboard-but-when-im-viewing-the-si
@@ -38,11 +51,11 @@ class WPA_Hawaiian_Howdy {
     $current_date_time = getdate();
     $current_hour = current_time( 'H' );
     $current_day = $current_date_time[ 'wday' ];
+
     $MESSAGE_MORNING = 'Aloha K&#xe4;kahiaka (Good Morning)';
     $MESSAGE_DAY = 'Aloha Awakea (Good Day)';
     $MESSAGE_AFTERNOON = "Aloha 'Auinal&#xe4; (Good Afternoon)";
     $MESSAGE_EVENING = 'Aloha Ahiahi (Good Evening)';
-
     $MESSAGE_DEFAULT = 'Aloha';
 
     /* Set the greeting based on time of day
@@ -69,6 +82,10 @@ class WPA_Hawaiian_Howdy {
         break;
     }
 
+    /* Add Happy Aloha Friday greeting if it is Friday.  This will be appended
+     * at the end of the message.  $current_day will be 0-6, with Sunday being
+     * 0.
+     */
     switch ( $current_day) {
       case ( 6 ):
         $message .= ' and Happy Aloha Friday';
@@ -81,5 +98,34 @@ class WPA_Hawaiian_Howdy {
     ) );
   } // End hawaiianize_howdy
 
+
+  function wporg_options_page_html()
+  {
+      // check user capabilities
+      if (!current_user_can('manage_options')) {
+          return;
+      }
+      ?>
+      <div class="wrap">
+          <h1><?= esc_html(get_admin_page_title()); ?></h1>
+          <form action="options.php" method="post">
+              <?php
+              // output security fields for the registered setting "wporg_options"
+              settings_fields('wporg_options');
+              // output setting sections and their fields
+              // (sections are registered for "wporg", each field is registered to a specific section)
+              do_settings_sections('wporg');
+              // output save settings button
+              submit_button('Save Settings');
+              ?>
+          </form>
+      </div>
+      <?php
+  }
+
+
+
 } // End class WPA_Hawaiian_Howdy
+
+
 $hawaiian_greetings = new WPA_Hawaiian_Howdy();
